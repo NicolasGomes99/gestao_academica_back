1929a148192649f84d0b903b10b84815d1688d2d
NicolasGomes99 <nicolasgabrielgomes9@gmail.com>
2025-03-26 17:06:13 -0300

    fix: relacionando corretamente Estudante com demais classes

--- src/main/java/br/edu/ufape/sguAuthService/comunicacao/dto/estudante/EstudanteRequest.java
+++ src/main/java/br/edu/ufape/sguAuthService/comunicacao/dto/estudante/EstudanteRequest.java
@@ -1,6 +1,9 @@
 package br.edu.ufape.sguAuthService.comunicacao.dto.estudante;
 
+import br.edu.ufape.sguAuthService.models.DadosBancarios;
+import br.edu.ufape.sguAuthService.models.Endereco;
 import br.edu.ufape.sguAuthService.models.Estudante;
+import br.edu.ufape.sguAuthService.models.TipoEtnia;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,6 +18,9 @@ public class EstudanteRequest {
     private String contatoFamilia;
     private boolean deficiente;
     private String tipoDeficiencia;
+    private TipoEtnia tipoEtnia;
+    private Endereco endereco;
+    private DadosBancarios dadosBancarios;
 
     public Estudante convertToEntity(EstudanteRequest estudanteRequest, ModelMapper modelMapper) {
         return modelMapper.map(estudanteRequest, Estudante.class);
--- src/main/java/br/edu/ufape/sguAuthService/comunicacao/dto/estudante/EstudanteResponse.java
+++ src/main/java/br/edu/ufape/sguAuthService/comunicacao/dto/estudante/EstudanteResponse.java
@@ -1,6 +1,9 @@
 package br.edu.ufape.sguAuthService.comunicacao.dto.estudante;
 
+import br.edu.ufape.sguAuthService.models.DadosBancarios;
+import br.edu.ufape.sguAuthService.models.Endereco;
 import br.edu.ufape.sguAuthService.models.Estudante;
+import br.edu.ufape.sguAuthService.models.TipoEtnia;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,6 +19,9 @@ public class EstudanteResponse {
     private String contatoFamilia;
     private boolean deficiente;
     private String tipoDeficiencia;
+    private TipoEtnia tipoEtnia;
+    private Endereco endereco;
+    private DadosBancarios dadosBancarios;
 
     public EstudanteResponse(Estudante estudante,  ModelMapper modelMapper) {
         if (estudante == null) throw new IllegalArgumentException("Estudante não pode ser nulo");
